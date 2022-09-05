fae722475e6b94d26b7d7c2bcebf8d787002cf95
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 19:09:45 +0530

    Update negativeElements.js

--- negativeElements.js
+++ negativeElements.js
@@ -1,28 +1,32 @@
-(function(){
-    //function pushNegativeToEnd(arr){
-        // array.sort((a,b))=> b-a
-        // console.log()
+(function() {
+    //  function pushNegativeToEnd(arr){
+    //     arr.sort((a,b) => b -a)
+    //     console.log("new Arr " +arr); // n logn
+    //  }
 
-
-    // }
-    function pushNegativeToEnd(){
-        let left =0,right = arr.length -1
-        while (left<=right){
-            if(arr[left]>=0 && arr[right]>=0){
+     function pushNegativeToEnd(arr){
+        let left =0, right = arr.length -1;
+        while(left <= right){
+            if(arr[left] >=0 && arr[right] >=0) {
                 left++;
+            } else if(arr[left] < 0 && arr[right] < 0) {
+               right--;
+            } else if(arr[left] < 0 && arr[right] >= 0) {
+                let temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            } else {
+                left++;
+                right--;
             }
-        
-        
-        if
-    }
+        }
+
+        return arr;
+     }
 
 
-
-
-    }
-        
-
-
-
-
+     let sample = [-1,1,8,-5,0,-3];
+     console.log(pushNegativeToEnd(sample))
 })();
