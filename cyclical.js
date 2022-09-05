9c661a27129f4da8232f12526ba11aefb17feb81
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 19:13:14 +0530

    Update cyclical.js

--- cyclical.js
+++ cyclical.js
@@ -1,17 +1,20 @@
-(function(){
-    function cyclical(arr){
-        let left =0; right = n-1;
-        while(left<=right){
+(function() {
+    function cyclical(arr, n) {
+        if(n == 0) return "Array is empty";
+        
+        let left = 0; right = n - 1;
+        while(left <= right) {
             let temp;
-            temp =arr[left]
+            temp = arr[left];
             arr[left] = arr[right];
-            arr[right]=temp;
-            left++
+            arr[right] = temp;
+            left++;
         }
-        return arr;
 
+        return arr;
     }
-    let sample =[4,21,90,0-1]
-    //output[-1,4,2,1,90,0]
-    console.log(cyclical(sample,sample.length))
+
+    let sample = [4,2,1,90,0,-1];
+    // output [-1, 4,2,1,90,0]
+    console.log(cyclical(sample, sample.length))
 })();
