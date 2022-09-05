3bf1102c0217f8352db5372b548a3c586444c118
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 18:59:36 +0530

    Update waveform.js

--- waveform.js
+++ waveform.js
@@ -1,27 +1,26 @@
-(function(){
-    function wave(matrix, total_cols, total_rows){
-        let col=0;
-     
-        while(col <= total_cols){
-            if(col % 2 ==0){
-                for(let row = 0; col < total_cols; col++){
-                console.log(matrix[row][col]);
-            }
-            } else {
-                for(let row =total_rows-1;row>=0;row--){
-                console.log(martix[row][col]);
-                }
-        
-            }
 
-            col++
-        } 
+(function(){
+
+    function wave(matrix, total_rows, total_cols){
+        let col= 0;
+        while(col <= total_cols ){ // 4
+            if(col % 2 == 0) {
+                for(let row = 0; row < total_rows; row++){
+                    console.log(matrix[row][col]);
+                } // N
+            } else {
+                for(let row = total_rows - 1; row >=0; row--){
+                    console.log(matrix[row][col]);
+                } // N
+            } 
+
+            col++;
+        }
     }
 
-        
-    
+    // external loop * internal_loop N
+    //M * (N)
 
-    let matrix = [[1,2,3,4],[,5,6,7,8],[9,10,11,12]]
-        console.log(wave(matrix, 3,4))
-
-})();
+    let matrix = [[1,2,3,4],[5,6,7,8], [9,10,11,12]]
+    console.log(wave(matrix, 3,4))
+})()
