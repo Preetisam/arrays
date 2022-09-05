fb334cd3e824b30cf87e02ee3850dc5a5e9d32bc
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 19:00:39 +0530

    Update diogonal.js

--- diogonal.js
+++ diogonal.js
@@ -1,21 +1,19 @@
 (function(){
-
-    let matrix=[[1,0,0],[0,1,0],[0,0,1]];
-
-    
-    console.log(matrix)
-    for (let i= 0 ;i<matrix.length;i++){
-        console.log(matrix[i]);
-        for(let j =0;j<matrix[i].length;j++){
-            console.log(matrix[i][j]);
-            if(i!=j && matrix[i][j]!=0){
-                return false;
-
+    function diagonal(matrix, rows, cols){
+        for(let row = 0; row < rows; row++){
+            for(let col =0; col < cols; col++) {
+                if(row != col && matrix[row][col] !=0) {
+                    return false;
+                }
             }
         }
+
         return true;
     }
-    console.log(matrix)
 
 
+    let matrix = [[0,0,0], [0,0,0], [0,0,0]];
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    console.log(diagonal(matrix,rows,cols))
 })();
