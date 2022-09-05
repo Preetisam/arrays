5ae3eee12959fc0fefa12d5b329c838f5eacde34
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 19:02:11 +0530

    Update find_xmatrix.js

--- find_xmatrix.js
+++ find_xmatrix.js
@@ -1,18 +1,22 @@
 (function(){
-    function search(matrix,row,col,numberToFind){
-        let
+    function search(matrix, rows, cols, numberToFind){
+        let row = 0, col = cols - 1 ;
+        while(row < rows && cols >=0) {
+            if(matrix[row][col] == numberToFind) {
+                return true;
+            }else if(matrix[row][col] < numberToFind) {
+                row++;
+            }else if(matrix[row][col] > numberToFind) {
+               col--
+            }
+        }
 
-
-
-
-
-
-
+        return false;
     }
-    let matrix =[[1,5,7,8],[9,11,13,19],[21,24,26,39]]
-    let rows=matrix.length;
-    let col=matrix[0].length;
-    let numberToFind =13;
-    console.log(search(matrix,rows,cols,numberToFind))
 
-})();
+    let matrix = [[1,5,7, 8],[9,11,13,19],[21,24,26,39]];
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    let numberToFind = 261;
+    console.log(search(matrix, rows, cols, numberToFind))
+})()
