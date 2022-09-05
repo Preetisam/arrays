5f970fbccde7a855e3536dc460fccf2dd5338e26
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 19:23:02 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,38 +1,27 @@
 (function(){
-    let numbers = [1,2,3,4,5];
-    //expected output[5,10,15,20,25];
-   
-    //mutiply by5 is call backfun
-     
-   
-    let multiples = numbers.map(function(value){
-        return value*5;
-    });
+    let numbers = [1,2,3,4,5,6,7,8,9,10];  
+    //expected output  [5,10,15,20,25];
+    // let multiples = numbers.map(function(value){
+    //     return value * 5;
+    // });
+
+    // Arrow functions =>
+    let multiples = numbers.map((value) => value * 5);
+
     console.log(multiples);
-    console.log(numbers);
-    //[5,10,15,20,25]
 
+    // expected output [1, 4, 9, 16, 25]
+    // let squares = numbers.map(function(number){
+    //     return number * number;
+    // })
 
-    // let mutiples = numbers.map((value) =>value *5);
-     // numbers.map(function(value,index,arr){});
-     
-    // numbers.map(multipleBy5);
-    // function multipleBy5(value,i,arr){
+     let squares = numbers.map((number) => number * number)
 
-    
-    
+    console.log("Squares are " + squares);
 
-    
-       
+    let names = ["Peter", "Tony", "Steve"];
+    let newNames = names.map((name) =>  "Mr " + name)
 
-    
-
-
-    
-
-
-
-
-
+    console.log(newNames);
 
 })();
