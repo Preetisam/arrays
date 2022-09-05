a5d6a27b3975334464ae8f4e23db60bb3b789550
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 19:08:43 +0530

    Update longestSequence.js

--- longestSequence.js
+++ longestSequence.js
@@ -1,27 +1,25 @@
 (function(){
-    function longestSequence(arr,n){
-        let count =1, max =1;
-        arr.sort((a,b) => a-b);
-        for (let i = 0; i<n; i++){
-            if(arr[i]==arr[i+1]){
-                countinue;
+    function longestSequence(arr, n){
+        let count = 1, max = 1;
+        arr.sort((a,b) => a - b);
+        for(let i = 0; i < n; i++) {
+            if(arr[i] == arr[i+1]){
+                continue;
             }
-            if(arr[i]+1== arr[i+1]){
+
+            if(arr[i] + 1 == arr[i + 1]) {
                 count++;
             }
-            else{
+            else {
                 max = count > max ? count : max;
-                count =1;
+                count = 1;
             }
-            
         }
-        console.log("The sorted array is");
+        console.log("The sorted array is " + arr);
 
         return max;
-
-
     }
-   let sample = [34,54,1,2,8,10,9,4,3];
-   console.log("longest sequence is " +longestSequence)
 
-})();
+    let sample = [34,54,1,2,8, 10, 9, 4,3,2];
+    console.log("longest sequence is " + longestSequence(sample, sample.length))
+})()
