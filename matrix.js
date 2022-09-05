9be5ee42ef7c15cdf32dea3ce464a013e646b7cd
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 18:55:05 +0530

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,20 +1,24 @@
-(function(){
+(function() {
 
-    let matrix=[[1,2,3],[4,5,6],[7,8,9]];
+  let arr1 = [1,1,1],
+      arr2 = [2,2,2],
+      arr3 = [3,3,3]
 
-    
-    console.log(matrix)
-    for (let i= 0 ;i<matrix.length;i++){
-        console.log(matrix[i]);
-        for(let j =0;j<matrix[i].length;j++){
-            console.log(matrix[i][j]);
-            if(i<=j){
-                i++
+   //let matrix = [arr1,arr2,arr3];
+   let matrix = [[1,1,1], [2,2,2], [3,3,3]]
 
-            }
-        }
+   // add item at the end
+  // matrix.push([0,0,0])
+
+  //remove item from the end
+   // matrix.pop()
+    // insert an item at 2nd index
+  // matrix.splice(2,0, [9,9,9]);
+
+   for(let i = 0; i< matrix.length;i++){
+    for(let j = 0; j < matrix[i].length;j++) {
+        console.log(matrix[i][j])
     }
-    console.log(matrix)
-
-
+   }
+  
 })();
