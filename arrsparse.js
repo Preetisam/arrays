7721b138ea90fa13137a82fa08adf3e8bcf27920
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-10-29 20:37:16 +0530

    Update arrsparse.js

--- arrsparse.js
+++ arrsparse.js
@@ -1,32 +1,21 @@
-(function(){
-    function checkIfSparse(arr){
+
+
+(function() {
+    function checkIfSparse(multiArr) {
         let counter = 0;
-        let rowOunt = multiArr.length;
-        let coulmnCount = 0;
-        for(let row =0; row < multiArr.length;row++){
-            columnCiunt += multiArr[row]
-            for(let col=0; col < multiArr[row].length;col++){       
-                if(multiArr[row][col]==0){
-                counter++;
+        let rowCount = multiArr.length;
+        let columnCount = 0
+        for(let row = 0; row < multiArr.length; row++) {
+            columnCount = multiArr[row].length;
+            for(let col=0; col < multiArr[row].length; col++) {
+                if(multiArr[row][col] == 0) {
+                    counter++;
                 }
             }
-
-           return (counter >parseInt((rowCount * columnCount)/2))
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         }
 
+        return counter > parseInt((rowCount * columnCount)/2);    }
 
-
-    }
-    let multidimensional =[[1,0,15,0][6,0,0,67],[1,0,45,0,]]
-    console.log("Is Array Sparse" + checkIfSparse(multidimensional))
+    let multidimensional = [[1,0,0,0], [6,0,0,67], [1,0,45,0],[1,0,0,0]]
+    console.log("Is Array Sparse " + checkIfSparse(multidimensional));
 })();
